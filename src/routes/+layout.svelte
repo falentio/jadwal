787c18c11565f557f1b7bca8ba61833e78997f02
falentio <kfalentio@proton.me>
2025-05-07 22:53:13 +0700

    chore: add posthog web analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
 	import "@fontsource-variable/montserrat";
 	import "../app.css";
-
+	import { browser } from "$app/environment";
+	import posthog from "posthog-js";
 	let { children } = $props();
+
+	if (browser) {
+		posthog.init("phc_MJpPOOZR09qGcZ5cnDojw1Kf3zycOdBjeQBsWNDSoO2", {
+			api_host: "https://eu.i.posthog.com",
+			person_profiles: "always", // or 'always' to create profiles for anonymous users as well
+		});
+	}
 </script>
 
 <div class="min-h-svh w-full flex flex-col antialiased">
